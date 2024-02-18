98da993de1a324575842a2e9c952b526b5d6d2f6
unknown <tyler.kd.knapp@gmail.com>
2024-02-18 13:55:51 -0600

    Added put route within category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -40,7 +40,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const locationData = await Category.create({
-      product_id: req.body.product_id,
+      category_name: req.body.category_name,
     });
     res.status(200).json(locationData);
   } catch (err) {
@@ -49,8 +49,22 @@ router.post('/', async (req, res) => {
 });
 
 // update a category by its 'id' value
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // delete a category by its 'id' value
